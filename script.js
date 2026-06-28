eabee9f06e4267148ff82a2ea120210c550925db
minar <mi.rezazade@gmail.com>
2026-06-28 14:20:25 +0330

    init

---
+++ script.js
@@ -0,0 +1,65 @@
+const deadline = new Date('2026-08-06T23:59:59');
+deadlineText.textContent = deadline.toLocaleDateString('fa-IR');
+
+let startButton = document.getElementById('start-button');
+startButton.addEventListener("click", function (){
+    if (new Date() > deadline) {
+        alert('⛔ وقت بازی تموم شده!');
+        return;
+    }
+    location.href='games.html';
+});
+// function startButtonClick() {
+//     if (new Date() > deadline) {
+//         alert('⛔ وقت بازی تموم شده!');
+//         return;
+//     }
+//     location.href='games.html';
+// }
+
+
+
+
+
+
+//
+// const games = [{t: 'معمای ریاضی', q: '۵+۸×۲', a: '21', s: 10}, {
+//     t: 'مورس',
+//     q: '.... . .-.. .-.. ---',
+//     a: 'hello',
+//     s: 15
+// }, {t: 'رمز سزار', q: 'KHOOR', a: 'HELLO', s: 20}];
+// let score = +localStorage.score || 0, done = JSON.parse(localStorage.done || '[]');
+// scoreEl = score;
+// document.getElementById('score').textContent = score;
+// start.onclick = () => {
+//     if (new Date() > deadline) {
+//         alert('⛔ وقت بازی تموم شده!');
+//         return;
+//     }
+//     landing.classList.add('hidden');
+//     games.classList.remove('hidden');
+//     render();
+// }
+//
+// function render() {
+//     cards.innerHTML = '';
+//     document.getElementById('score').textContent = score;
+//     games.forEach((g, i) => {
+//         let c = document.createElement('div');
+//         c.className = 'card' + (done.includes(i) ? ' done' : '');
+//         c.innerHTML = `<h3>${g.t}</h3><p>+${g.s} امتیاز</p><button ${done.includes(i) ? 'disabled' : ''}>شروع</button>`;
+//         c.querySelector('button').onclick = () => {
+//             let ans = prompt(g.q);
+//             if (ans && ans.toLowerCase() == g.a.toLowerCase()) {
+//                 alert('درست!');
+//                 score += g.s;
+//                 done.push(i);
+//                 localStorage.score = score;
+//                 localStorage.done = JSON.stringify(done);
+//             } else alert('اشتباه');
+//             render();
+//         };
+//         cards.appendChild(c);
+//     });
+// }
